3c1daba8325a1b142c6b2fcd8f12e89db54aae2f
bxumsle000-cmd <bxumsle000@gmail.com>
2026-07-29 16:22:14 +0800

    query

---
+++ src/main/resources/cash.sql
@@ -0,0 +1,17 @@
+USE [jdbc]
+GO
+
+/****** 物件:  Table [dbo].[cash]    指令碼日期: 2026/7/29 下午 04:20:36 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[cash](
+	[name] [nchar](10) NOT NULL,
+	[deposit] [int] NULL
+) ON [PRIMARY]
+GO
+
+
